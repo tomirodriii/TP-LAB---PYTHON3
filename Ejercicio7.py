3e464bc5eb7ac04c6b7a898378aeaa509b15c13c
Tomas Rodriguez <franco.tomas.rodriguez@gmail.com>
2026-03-26 08:45:48 -0300

    Ejercicio 7

--- Ejercicio7.py
+++ Ejercicio7.py
@@ -2,3 +2,26 @@
 #7. Integrador: Crear programa que permita al usuario agregar tareas con descripción, 
 # fecha límite y prioridad, así como mostrar  la lista de tareas. 
 # Este menú se repite hasta que el usuario elige salir."
+
+import os
+os.system("cls")
+
+lista_tareas = []  
+
+seguir = True
+while seguir:
+    tarea = input("Ingrese el nombre de la tarea:")
+    descripcion = input("Ingrese la descripción de la tarea:")
+    fecha_limite = input("Ingrese la fecha límite de la tarea:")
+    prioridad = input("Ingrese la prioridad de la tarea:")
+    
+    lista_tareas.append([tarea, descripcion, fecha_limite, prioridad])
+    
+    print(lista_tareas)
+    
+    opcion = input("¿Desea agregar otra tarea? (Si/No):").lower()
+    
+    if opcion == "no":
+        seguir = False
+        
+        
