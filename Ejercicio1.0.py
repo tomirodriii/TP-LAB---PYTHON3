12abb4364c5714c4a9e493a7c3a864455721b4b3
Tomas Rodriguez <franco.tomas.rodriguez@gmail.com>
2026-03-26 08:02:01 -0300

    Ejercicios

---
+++ Ejercicio1.0.py
@@ -0,0 +1,6 @@
+# 1. Define una lista de diccionarios que represente información personal. nombre,edad. Luego, accede a
+# elementos específicos de la lista, como el primer diccionario, el nombre de la primera persona y la edad de la 
+# segunda persona, para finalmente imprimir los resultados en la consola.
+
+import os
+os.system("cls")
---
+++ Ejercicio1.2.py
@@ -0,0 +1,4 @@
+# 1.2. Del punto 1, recorrer y mostrar k,v
+
+import os
+os.system("cls")
