742290296a3ef6382024b9e66fc772de10c2f830
Tomas Rodriguez <franco.tomas.rodriguez@gmail.com>
2026-03-30 16:05:51 -0300

    Ejercicio7

--- Ejercicio7.py
+++ Ejercicio7.py
@@ -6,22 +6,43 @@
 import os
 os.system("cls")
 
-lista_tareas = []  
+def agregar_tarea(tareas, tarea, fecha_limite, prioridad):
+    nueva_tarea = {"Tarea": tarea, "Fecha limite": fecha_limite, "Prioridad": prioridad}
+    tareas.append(nueva_tarea)
+    print("Tarea agregada exitosamente.")
 
-seguir = True
-while seguir:
-    tarea = input("Ingrese el nombre de la tarea:")
-    descripcion = input("Ingrese la descripción de la tarea:")
-    fecha_limite = input("Ingrese la fecha límite de la tarea:")
-    prioridad = input("Ingrese la prioridad de la tarea:")
-    
-    lista_tareas.append([tarea, descripcion, fecha_limite, prioridad])
-    
-    print(lista_tareas)
-    
-    opcion = input("¿Desea agregar otra tarea? (Si/No):").lower()
-    
-    if opcion == "no":
-        seguir = False
-        
-        
+
+def mostrar_tareas(tareas):
+    if not tareas:
+        print("No hay tareas pendientes.")
+    else:
+        for i, tarea in enumerate(tareas, 1):
+            print(f"\nTarea {i}:")
+            for clave, valor in tarea.items():
+                print(f"{clave}: {valor}")
+
+
+if __name__ == "_main_":
+    lista_tareas = []
+
+    while True:
+        print("\n1. Agregar tarea")
+        print("2. Mostrar tareas")
+        print("3. Salir")
+
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            tarea_nueva = input("Ingrese la descripción de la tarea: ")
+            fecha_limite_nueva = input("Ingrese la fecha limite (formato: dd/mm/yyyy): ")
+            prioridad_nueva = input("Ingrese la prioridad: ")
+            agregar_tarea(lista_tareas, tarea_nueva, fecha_limite_nueva, prioridad_nueva)
+
+        elif opcion == "2":
+            mostrar_tareas(lista_tareas)
+
+        elif opcion == "3":
+            break
+
+        else:
+            print("Opción no válida. Intente nuevamente.")
